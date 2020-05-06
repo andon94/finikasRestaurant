2fabd970aff5b1174c7db4c8b0c881f9f4025847
andon94 <andonovic.bob@gmail.com>
2020-05-06 20:49:00 +0200

    added gbuild to package.json.scripts and put back env-cmd to develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,3 @@
-// let activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
-
-
-// require("dotenv").config({
-//   path: `.env.${process.env.NODE_ENV}`,
-// })
-
 module.exports = {
   siteMetadata: {
     title: 'Finikas Restaurant',
