a5fdb8a462f21c4f96f706427961b030535211bc
andon94 <andonovic.bob@gmail.com>
2020-05-15 23:12:51 +0200

    keywords and style

--- src/components/head.js
+++ src/components/head.js
@@ -38,7 +38,7 @@ const Head = ({ title, description, url, img, keywords }) => {
                 const metaTitle = title || data.site.siteMetadata.title
                 const metaUrl = url || data.site.siteMetadata.url
                 const metaImg = img || data.site.siteMetadata.img
-                const metaKeywords = keywords || ["golden beach restaurant", "best restaurant thassos", "finikas restaurant", "finikas taverna", "thassos taverna", "golden beach taverna", "fresh seafood"]
+                const metaKeywords = keywords || ["golden beach restaurant", "best restaurant thassos", "finikas taverna", "thassos taverna", "golden beach taverna", "where to eat thassos", "where to eat golden beach", "traditional greek food", "golden beach takeaway", "good food golden beach", "family restaurant thassos", "golden beach restoran", "tasos restoran", "tasos dobra kafana", "tasos dobra hrana", "thassos mussaka", "thassos papucaki", "добра храна", "добър ресторант на Тасос", "добра механа", "вкусна храна", "mâncare gustoasă Thassos", "unde să mănânci golden beach", "tava finikas", "tava thassos", "golden beach camping"]
                 return (
                     <Helmet
                         title={title}
